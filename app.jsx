07ebe986264adef3eabb113c0dd12ce9e87fe929
Amil <amilsil@gmail.com>
2020-06-03 09:01:38 +1000

    Initial Commit

---
+++ app.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<div>This works and changed</div>, document.getElementById("root"));
---
+++ webpack.config.app.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const AddAssetHtmlPlugin = require("add-asset-html-webpack-plugin");
+
+module.exports = {
+    context: __dirname,
+    entry: path.join(__dirname, "app.jsx"),
+    devtool: "#source-map",
+    mode: "development",
+    output: {
+        path: path.join(__dirname, "build"),
+        filename: "[name].js",
+        chunkFilename: "[name].js"
+    },
+    plugins: [
+        new webpack.DllReferencePlugin({
+            context: __dirname,
+            manifest: require("./vendor/vendor-manifest.json")
+        }),
+        new HtmlWebpackPlugin({
+            template: "./index.html",
+            filename: "./build/index.html"
+        }),
+        new AddAssetHtmlPlugin({
+            filepath: path.resolve(__dirname, "./build/vendor.js")
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+};
